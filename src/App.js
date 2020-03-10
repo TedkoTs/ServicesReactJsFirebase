b21c4f022e37c6c43c9707257b5d6ff7a3abd5ef
Teodor Tsvetanov <teodor.tzvetanov@gmail.com>
2020-03-10 14:05:28 +0200

    part14

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HomePage from './pages/home';
+import Sidebar from './components/sidebar'
 
 function App() {
   return (
     <div> 
     <HomePage />
+    <Sidebar />
      </div>
   );
 }
