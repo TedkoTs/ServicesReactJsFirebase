f37fd5788b4ff7d888a85b228a9dbf830690f58d
Teodor Tsvetanov <teodor.tzvetanov@gmail.com>
2020-03-10 16:43:40 +0200

    part 18

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
-import NavbarClone from "./components/navbarClone";
 
 import HomePage from "./pages/home";
+import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
+import FaqPage from "./pages/Faq";
+import ProfilePage from "./pages/Profile";
+import ServicesPage from "./pages/Services";
+import LoginPage from "./pages/Login";
+import RegisterRage from "./pages/Register";
+
+
 
 function App() {
   return (
     <div>
       <Router>
         <Navbar />
-        <NavbarClone />
+        <Navbar id="navbar-clone" />
         <Sidebar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <HomePage />
+          </Route>
+          <Route path="/faq">
+            <FaqPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/register">
+            <RegisterRage />
+          </Route>
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
+          <Route path="/services">
+            <ServicesPage />
           </Route>
         </Switch>
       </Router>
---
+++ src/pages/Register.js
@@ -0,0 +1,6 @@
+
+import React from "react";
+
+const Register = () => <h1>Register page</h1>;
+
+export default Register;
