dbd1b041583e4bea003a3e57e6d53bd770813785
Teodor Tsvetanov <teodor.tzvetanov@gmail.com>
2020-03-10 15:22:42 +0200

    part 16

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,8 @@ import React from "react";
 import Navbar from "../components/navbar";
 import NavbarClone from "../components/navbarClone";
 import { getServices } from "../store/index";
+import ServiceItem from "../components/service/serviceItem";
+import Hero from "../components/hero";
 
 class Home extends React.Component {
   state = {
@@ -16,27 +18,7 @@ class Home extends React.Component {
 
   renderServices = services => {
     return services.map(service => (
-      <div key={service.id} className="column is-one-third">
-        <div
-          className="feature-card is-bordered has-text-centered revealOnScroll delay-1"
-          data-animation="fadeInLeft"
-        >
-          <div className="card-title">
-            <h4>{service.title}</h4>
-          </div>
-          <div className="card-icon">
-            <img src={service.image} alt="" />
-          </div>
-          <div className="card-text">
-            <p>{service.description}</p>
-          </div>
-          <div className="card-action">
-            <a href="#" className="button btn-align-md accent-btn raised">
-              Learn More
-            </a>
-          </div>
-        </div>
-      </div>
+      <ServiceItem key={service.id} service={service} />
     ));
   };
 
@@ -45,37 +27,7 @@ class Home extends React.Component {
       <div>
         <Navbar />
         <NavbarClone />
-        <section className="hero is-default is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <div className="columns is-vcentered">
-                <div className="column is-5 is-offset-1 landing-caption">
-                  <h1 className="title is-1 is-bold is-spaced">
-                    Learn, Collaborate.
-                  </h1>
-                  <h2 className="subtitle is-5 is-muted">
-                    Lorem ipsum sit dolor amet is a dummy text used by
-                    typography industry{" "}
-                  </h2>
-                  <p>
-                    <a className="button cta rounded primary-btn raised">
-                      Get Started
-                    </a>
-                  </p>
-                </div>
-                <div className="column is-5 is-offset-1">
-                  <figure className="image is-4by3">
-                    <img
-                      src={process.env.PUBLIC_URL + "/worker.svg"}
-                      alt="Description"
-                    />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <Hero />
         <section className="section section-feature-grey is-medium">
           <div className="container">
             <div className="title-wrapper has-text-centered">
